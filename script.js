700664fb42b63f0eaadcb1dac3fb7fa64c77eddf
Geancarlo Muñoz <geancarlosmz@hotmail.com>
2021-07-08 06:13:13 +0000

    proyecto

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //Obtenemos los valores ingresados por el usuario
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    //calculamos el interes
+    var interest = principal * years * rate /100;
+    var year = new Date.getFullYear()+partseInt(years);
     
+    //mostrar resultado en pantalla
+    document.getElementById("result").innerHTML = "If you deposit \<mark\>" + principal + 
+    "\<\\mark\>\<br\>at an interes rate of \<mark\>" + rate + "%\<\\mark\>\<br\>You will receive an amount of \<mark\>" + amount +
+    "\<\\mark\>\<br\>in the year \<mark\>" + year + "\<\\mark\>\<br\>"; 
+
 }
+
+function updateRate()
+{
+    //obtenemos el valor del meter y lo mostramos en el span
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validar(){
+    //obtener el valor principal cuando pierde el foco
+    var principal = document.getElementById("principal").value;
+
+    //si el valor de principal es menos o igual a cero mostar mensaje
+    if(parseFloat(principal) <= 0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+}
+
         
